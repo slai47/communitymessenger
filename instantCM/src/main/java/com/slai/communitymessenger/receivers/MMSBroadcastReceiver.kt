ec56c65c1f53494a21813aa03a12e03c161c7f26
Harrison Frede <harrisonfrede@gmail.com>
2018-11-05 15:18:55 -0500

    Adding eventual MMS logging to make it work

--- instantCM/src/main/java/com/slai/communitymessenger/receivers/MMSBroadcastReceiver.kt
+++ instantCM/src/main/java/com/slai/communitymessenger/receivers/MMSBroadcastReceiver.kt
@@ -4,13 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
+import android.util.Log
 
 class MMSBroadcastReceiver : BroadcastReceiver() {
 
 
     override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("MMSBroadcastReceiver", intent?.action + " " + intent?.extras.toString())
         if(intent?.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
-
         }
     }
 }
